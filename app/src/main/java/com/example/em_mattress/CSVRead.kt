23fa7b0a3751bcd9a0c8f4add1937c669643f300
johnrmcp <johnmcp124@gmail.com>
2024-10-22 17:23:15 -0400

    Attempted CSV Addition

--- app/src/main/java/com/example/em_mattress/CSVRead.kt
+++ app/src/main/java/com/example/em_mattress/CSVRead.kt
@@ -1,6 +1,9 @@
-package com.example.em_mattress
+ package com.example.em_mattress
 
+import android.net.Uri
 import android.widget.EditText
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -31,7 +34,9 @@ import java.io.InputStream
 
 @Composable
 fun CSVReadScreen(navController: NavController, modifier: Modifier = Modifier) {
-    var text by remember{mutableStateOf("")}    //delete if replaced with file input field
+    //var text by remember{mutableStateOf("")}    //delete if replaced with file input field
+    var selectedFilePath by remember { mutableStateOf("") }
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -46,25 +51,54 @@ fun CSVReadScreen(navController: NavController, modifier: Modifier = Modifier) {
                 modifier = modifier,
                 fontSize = MaterialTheme.typography.headlineSmall.fontSize
             )
+            /*
             OutlinedTextField( //see var declaration
                     value = text,
                     onValueChange = {text = it },
                     label = { Text("Label") }
             )
+            */
+            FilePicker(onFileSelected = { uri ->
+                    selectedFilePath = uri.path
+            })
+
+            Text(text = "Selected File: $selectedFilePath")
+            /*
             Button(
                 onClick = { navController.navigate(route = Screen.DataOutput.route) },
                 content = { Text("Read CSV", fontSize = MaterialTheme.typography.headlineSmall.fontSize) },
                 modifier = Modifier.padding(top = 16.dp),
             )
+            */
+
             Text(
                 text = "CSV Read",
                 modifier = modifier,
                 fontSize = MaterialTheme.typography.headlineSmall.fontSize
             )
-
         }
     }
 }
+
+ //composable function which defines the file picker component
+ @Composable
+ fun FilePicker(onFileSelected: (Uri) -> Unit) {
+     val launcher = rememberLauncherForActivityResult(
+         ActivityResultContracts.GetContent()
+     ) { uri ->
+         if (uri != null) {
+             onFileSelected(uri)
+         } else {
+             println("No file selected")
+         }
+     }
+
+     Button(onClick = {
+         launcher.launch("text/csv") // Specify the MIME type for CSV files
+     }) {
+         Text(text = "Select CSV File")
+     }
+ }
 
 @Preview(showBackground = true)
 @Composable
